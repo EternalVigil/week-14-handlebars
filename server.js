127b1540e5847814c033c1e937c3cae7d99f2f0a
EternalVigil <theeternalvigil@gmail.com>
2017-01-21 17:59:59 -0800

    modified server and package files

--- server.js
+++ server.js
@@ -1,34 +1,25 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 var app = express();
 var PORT = process.env.PORT || 3000;
-var db = require("/models");
 
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({
-	defaultLayout: "main"
-}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Import routes and give the server access to them.
+var routes = require("./controllers/controller.js");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({
-	type: "application/vnd.api+json"
-}));
+app.use("/", routes);
 
-app.use(express.static("./public"));
-
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
-
-db.sequelize.sync().then(function () {
-	"use strict";
-	app.listen(PORT, function () {
-		console.log("App listening on PORT " + PORT);
-	});
-});
+app.listen(PORT);
